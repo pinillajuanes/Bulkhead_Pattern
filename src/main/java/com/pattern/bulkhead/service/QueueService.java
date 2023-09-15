340aac487b1f10a02b956b9ab3533974bf574db9
juanadames1 <juancamilo.adames@hotmail.com>
2023-09-15 16:56:24 -0500

    Fix & Update: Control of connections to DB and making a Semaphore

--- src/main/java/com/pattern/bulkhead/service/QueueService.java
+++ src/main/java/com/pattern/bulkhead/service/QueueService.java
@@ -2,17 +2,15 @@ package com.pattern.bulkhead.service;
 
 import org.springframework.stereotype.Service;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.concurrent.Semaphore;
 
 @Service
 public class QueueService {
-
     private static final String DB_URL = "jdbc:sqlite:queue.db";
     private static final String CREATE_TABLE_SQL = "CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, message TEXT)";
+    private static final int MAX_DB_CONNECTIONS = 5;
+    private final Semaphore connectionSemaphore = new Semaphore(MAX_DB_CONNECTIONS, true);
 
     public QueueService() {
         initializeDatabase();
@@ -29,38 +27,53 @@ public class QueueService {
     }
 
     public void enqueue(String message) throws InterruptedException {
-        try (Connection connection = DriverManager.getConnection(DB_URL)) {
-            String insertSql = "INSERT INTO messages (message) VALUES (?)";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(insertSql)) {
-                preparedStatement.setString(1, message);
-                preparedStatement.executeUpdate();
+        try {
+            connectionSemaphore.acquire();
+            try (Connection connection = DriverManager.getConnection(DB_URL)) {
+                String insertSql = "INSERT INTO messages (message) VALUES (?)";
+                try (PreparedStatement preparedStatement = connection.prepareStatement(insertSql)) {
+                    preparedStatement.setString(1, message);
+                    preparedStatement.executeUpdate();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                connectionSemaphore.release();
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
     }
 
     public String dequeue() throws InterruptedException {
-        try (Connection connection = DriverManager.getConnection(DB_URL)) {
-            String selectSql = "SELECT * FROM messages LIMIT 1";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(selectSql)) {
-                try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                    if (resultSet.next()) {
-                        String message = resultSet.getString("message");
-                        String deleteSql = "DELETE FROM messages WHERE id = ?";
-                        try (PreparedStatement deleteStatement = connection.prepareStatement(deleteSql)) {
-                            deleteStatement.setInt(1, resultSet.getInt("id"));
-                            deleteStatement.executeUpdate();
+        try {
+            connectionSemaphore.acquire();
+            try (Connection connection = DriverManager.getConnection(DB_URL)) {
+                String selectSql = "SELECT * FROM messages LIMIT 1";
+                try (PreparedStatement preparedStatement = connection.prepareStatement(selectSql)) {
+                    try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                        if (resultSet.next()) {
+                            String message = resultSet.getString("message");
+                            String deleteSql = "DELETE FROM messages WHERE id = ?";
+                            try (PreparedStatement deleteStatement = connection.prepareStatement(deleteSql)) {
+                                deleteStatement.setInt(1, resultSet.getInt("id"));
+                                deleteStatement.executeUpdate();
+                            }
+                            return message;
+                        } else {
+                            return "No messages in the queue";
                         }
-                        return message;
-                    } else {
-                        return "No messages in the queue";
                     }
                 }
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return "Error reading from the queue";
+            } finally {
+                connectionSemaphore.release();
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return "Error reading from the queue";
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return "Interrupted";
         }
     }
 }
